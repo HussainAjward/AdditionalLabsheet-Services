e937d418ff618354a8c5be3259b011b00da54bce
Hussain Ajward M.N.M <it20192396@my.sliit.lk>
2021-08-24 08:44:52 +0530

    First commit

---
+++ app/src/main/java/com/example/androidservices/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.androidservices;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.media.MediaPlayer;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.provider.Settings;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MediaPlayer player =
+                MediaPlayer.create(this, Settings.System.DEFAULT_NOTIFICATION_URI);
+        player.setLooping(true);
+        player.start();
+    }
+
+}
